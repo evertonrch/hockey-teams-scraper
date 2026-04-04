bd6211e288852328eaef818fd7f4387ae721cc8c
Everton Rocha <imevertonrch@gmail.com>
2026-04-04 02:07:24 -0300

    gerando seed ddl

--- dao/team_dao.py
+++ dao/team_dao.py
@@ -1,12 +1,24 @@
 import logging
+from pathlib import Path
 from config.db_config import get_connection
 from model.team import Team
 
 log = logging.getLogger(__name__)
 
+ROOT = Path(__file__).resolve().parent.parent
+ddl_file = ROOT / "ddl.sql"
+
+def create_table():
+    with open(ddl_file, "r") as file:
+        with get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(file.read())
+
 def save(team: Team) -> None:
     with get_connection() as conn:
         with conn.cursor() as cursor:
+            create_table()
+            
             sql = """
                 INSERT INTO tb_hockei_team(
                     name, year, wins, losses, ot_losses, win_percent, goals_for, goals_against, diff
