f891747c4b9337b602564d331a2bcf1a6fa62238
Everton Rocha <imevertonrch@gmail.com>
2026-04-04 00:30:16 -0300

    utilizando contextos

--- dao/team_dao.py
+++ dao/team_dao.py
@@ -5,25 +5,19 @@ from model.team import Team
 log = logging.getLogger(__name__)
 
 def save(team: Team) -> None:
-    conn = get_connection()
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            sql = """
+                INSERT INTO tb_hockei_team(
+                    name, year, wins, losses, ot_losses, win_percent, goals_for, goals_against, diff
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s 
+                )
+            """.strip()
 
-    sql = """
-        INSERT INTO tb_hockei_team(
-            name, year, wins, losses, ot_losses, win_percent, goals_for, goals_against, diff
-        ) VALUES (
-            %s, %s, %s, %s, %s, %s, %s, %s, %s 
-        )
-    """.strip()
-
-    try:
-        log.info(f"inserindo dados do time: {team.name} no banco...")
-        cursor.execute(sql, tuple(team.__dict__.values()))
-        conn.commit()
-    except Exception as ex:
-        log.error("erro ao inserir no banco: ", str(ex))
-    
-    cursor.close()
-    conn.close()
-
-    
+            try:
+                log.info(f"inserindo dados do time: {team.name} no banco...")
+                cursor.execute(sql, tuple(team.__dict__.values()))
+                conn.commit()
+            except Exception as ex:
+                log.error("erro ao inserir no banco: %s", ex)
